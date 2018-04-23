2dca687cfd644e8eab18ed198543f256e63c0421
acruz1992 <andrew.cruz1@yahoo.com>
2018-04-23 07:34:35 -0700

    Signed-off-by: acruz1992 <andrew.cruz1@yahoo.com>

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -19,17 +19,28 @@ int main(int argc, char** argv) {
     char ch;                            //to have the user press enter between card draws
     char hit;                           //to have the user draw a 3rd card
     string name;                        //string to enter the users name
+    const int SIZE=20;
+    char fileOut[SIZE]="BlackjackOut.dat";
+    string fileIn="BlackjackIn.dat";
+    ofstream out;
+    ifstream in;
+    out.open(fileOut);
+    in.open(fileIn.c_str());
+    
     cout<<"\t\tBlackjack\n\n"<<endl;
     cout<<"welcome to blackjack. What is your first name? "<<endl;
     cin>>name;
     cout<<"How many games would you like to play?: ";
     cin>>choice;
+    cin.ignore();                       //had to place this to allow the program have different kinds of inputs
     cout<<endl;
     do{                                 //do while loop for the number of games to play
+        {
         cout<<name<<"'s hand:"<<endl;
         int pcard1=(rand()%(14-2+1))+2;  //random dealer card 1
         int pcard2=(rand()%(14-2+1))+2;  //random dealer card 2
-        switch (pcard1)
+        
+                switch (pcard1)
                 {
                     case 11: cout<<setw(10)<<"Ace";
                     break;
@@ -94,6 +105,10 @@ int main(int argc, char** argv) {
                         }
                     }while ((!(hit=='n'||hit=='N'))||!pctotal>=21);
                 }
+        }
+        cin.ignore();
+        cout<<"Press enter to continue."<<endl;
+        cin.get(ch);
         cout<<"Now the dealers turn."<<endl;
         {
         cout<<"Dealers hand: "<<endl;
@@ -160,6 +175,7 @@ int main(int argc, char** argv) {
                         }
         }
         cout<<endl;
+        
         //possible outcomes of the points from under the 21 score to over the 21 score
         {if(dctotal>21&&pctotal>21)
             cout<<"Both of you lost";
@@ -176,7 +192,11 @@ int main(int argc, char** argv) {
         }
         cout<<endl;
         cout<<"Press enter to continue."<<endl;
-        cin.get(ch);     
+        cin.get(ch);
+        
+            
     }while(--choice>=1);
+    out.close();
+    in.close();
     return 0;
 }
