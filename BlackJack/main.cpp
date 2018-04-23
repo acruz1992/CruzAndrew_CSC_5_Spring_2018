2f082f45d9512b2a160d45ff639cba987209946b
acruz1992 <andrew.cruz1@yahoo.com>
2018-04-22 18:58:18 -0700

    Signed-off-by: acruz1992 <andrew.cruz1@yahoo.com>

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -6,11 +6,18 @@
  */
 
 //System Libraries
-#include <iostream>     
+#include <iostream>
+#include <cstdlib>      //Library set random number seed
+#include <ctime>        //Libary to seed the random number generator
+#include <iomanip>      //Format Library
+#include <fstream>      //File Library
+#include <string.h>     //String Library
 using namespace std;   
-
-
 int main(int argc, char** argv) {
-   
+   int choice;                         //to chose how many games the user would like to play
+    float pctotal,dctotal;              //the total amount of value of he cards
+    char ch;                            //to have the user press enter between card draws
+    char hit;                           //to have the user draw a 3rd card
+    string name;                        //string to enter the users name
     return 0;
 }
